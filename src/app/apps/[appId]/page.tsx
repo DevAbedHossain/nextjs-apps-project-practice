7785a7cfe3d7bfdafb9491a08db1fa5902b955e0
DevAbedHossain <devabedhossain@gmail.com>
2026-09-22 11:22:38 +0600

    Apps single page params await set

--- src/app/apps/[appId]/page.tsx
+++ src/app/apps/[appId]/page.tsx
@@ -18,7 +18,7 @@ interface AppDetailsProms {
 
 const AppDetailsPage = async ({ params }: AppDetailsProms) => {
 
-    const { appId } = params;
+    const { appId } = await params;
 
     const apps = await getAllApps();
 
