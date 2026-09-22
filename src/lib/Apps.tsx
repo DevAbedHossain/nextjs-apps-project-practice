633c946a3d45caabcf6940c79875475411db7091
DevAbedHossain <devabedhossain@gmail.com>
2026-09-22 10:38:09 +0600

    Fixed

--- src/lib/Apps.tsx
+++ src/lib/Apps.tsx
@@ -1,5 +1,10 @@
 export const getAllApps = async () => {
     const res = await fetch(`${process.env.NEXT_PUBLIC_APPS_SERVER_URL}/data.json`, { next: { revalidate: 5 } });
+
+    if (!res.ok) {
+        throw new Error(`Failed to fetch apps: ${res.status}`);
+    }
+
     const data = await res.json();
     return data.apps;
 }
