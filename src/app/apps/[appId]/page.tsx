82ac27ebe84bf7fbe89ba63dc14b18fa4e15a697
DevAbedHossain <devabedhossain@gmail.com>
2026-09-22 10:27:04 +0600

    Single apps page type

--- src/app/apps/[appId]/page.tsx
+++ src/app/apps/[appId]/page.tsx
@@ -6,7 +6,7 @@ import { notFound } from "next/navigation";
 export const generateStaticParams = async () => {
     const getApps = await getAllApps()
 
-    return getApps.map((app: IApps) => ({ appId: app.id.toString() }))
+    return getApps.map((app: IApps) => ({ appId: app.id }))
 }
 
 interface AppDetailsProms {
